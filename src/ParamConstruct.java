691bed622c6d81d65130cce93dd7a3e0e06ba988
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-10-18 19:16:27 +0530

    parameter-constructor

--- src/ParamConstruct.java
+++ src/ParamConstruct.java
@@ -7,10 +7,14 @@ public class ParamConstruct {
 	ParamConstruct(int age){
 		System.out.println("Age of parameter Constructor:"+age);
 	}
-
+	ParamConstruct(int age,String nm){
+		System.out.println("Age of parameter Constructor "+nm+":"+age);
+	}
+	
 	public static void main(String[] args) {
 		ParamConstruct p1=new ParamConstruct();
 		ParamConstruct p2=new ParamConstruct(23);
+		ParamConstruct p3=new ParamConstruct(13,"p3");
 	}
 
 }
