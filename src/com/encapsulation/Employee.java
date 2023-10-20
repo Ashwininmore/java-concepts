e0fb5a88d76936376787cd7eb1e14867ff627704
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-10-20 20:28:37 +0530

    encapsulation

---
+++ src/com/encapsulation/Employee.java
@@ -0,0 +1,30 @@
+package com.encapsulation;
+
+public class Employee {
+	private int empid=123;
+	private String empname="Bahubali";
+	private String empadress="Mahismati";
+	
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getEmpadress() {
+		return empadress;
+	}
+	public void setEmpadress(String empadress) {
+		this.empadress = empadress;
+	}
+	
+	
+	
+
+}
---
+++ src/com/encapsulation/MyClass.java
@@ -0,0 +1,11 @@
+package com.encapsulation;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		
+
+	}
+
+}
