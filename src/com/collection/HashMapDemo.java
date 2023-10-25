b4fb9afadc03fd0ee3cf82257dbc0cba1eb8a3d4
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-10-25 20:33:04 +0530

    HashMap

--- src/com/collection/HashMapDemo.java
+++ src/com/collection/HashMapDemo.java
@@ -1,6 +1,9 @@
 package com.collection;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HashMapDemo {
 
@@ -8,12 +11,20 @@ public class HashMapDemo {
 		HashMap<Integer, String> hm= new HashMap();
 		hm.put(100, "Ashwini");
 		hm.put(103, "minsmol");
+		hm.put(107, "Ashu");
 		
 //		hm.put("Rakesh", 34);//gives error bcz generic
 		System.out.println(hm);
 		System.out.println(hm.get(103));
 		System.out.println(hm.containsKey(108));
 		System.out.println(hm.isEmpty());
+		//can't use iterator directly
+		Set<Entry<Integer,String>> itr=hm.entrySet();
+		Iterator<Entry<Integer,String>> result=itr.iterator();
+		while(result.hasNext()) {
+			System.out.println(result.next());
+//			<Entry<Integer,String>> finalresult=itr.iterator();
+		}
 	}
 
 }
