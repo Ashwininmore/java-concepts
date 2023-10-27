0a78d1470e60552efa54cd698e7270c3f372e288
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-10-27 19:53:29 +0530

    Math-class

---
+++ src/classespredefined/MathDemo.java
@@ -0,0 +1,18 @@
+package classespredefined;
+
+public class MathDemo {
+
+	public static void main(String[] args) {
+		double a=78;
+		double b=4;
+		
+		double c=60;
+		System.out.println("Max no:"+Math.max(a, b));
+		System.out.println("Square root of b"+Math.sqrt(b));
+		System.out.println("Power of a,b:"+Math.pow(a, b));
+		System.out.println("Log of a:"+Math.log(a));
+		System.out.println("exponential of b:"+Math.exp(b));
+		System.out.println("sin value of c:"+Math.sin(c));
+	}
+
+}
--- src/com/dateandtime/LeapDemo.java
+++ src/com/dateandtime/LeapDemo.java
@@ -1,9 +1,12 @@
 package com.dateandtime;
 
+import java.util.Scanner;
+
 public class LeapDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
 
 	}
 
