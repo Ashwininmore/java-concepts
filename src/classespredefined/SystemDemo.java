ead92fd2d6dca6c68cde188be9336f27426392b9
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-10-27 19:59:38 +0530

    system-class

---
+++ src/classespredefined/SystemDemo.java
@@ -0,0 +1,25 @@
+package classespredefined;
+
+public class SystemDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Library name os:"+System.mapLibraryName("os.name"));
+		System.out.println("version name os:"+System.mapLibraryName("os.version"));
+		System.out.println("Millisec:"+System.currentTimeMillis());
+		String name="Keerti"+System.lineSeparator()+"Suresh";
+		System.out.println(name);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
